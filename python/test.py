dd733847a58b6c7ebc77b77523ea6b806646507b
Benoit <benoit.leguay@gmail.com>
2019-01-09 13:04:42 +0100

    node js communicator works !

--- python/test.py
+++ python/test.py
@@ -1,9 +1,13 @@
 import sys
+import json
 
 def test():
 	return "works on python"
 
 if __name__ == "__main__":
-    functions = {'test': test}
-    res_json = functions[sys.argv[1]]()
-    print(res_json)
+    #functions = {'test': test}
+    #res_json = functions[sys.argv[1]]()
+
+    clothes = json.loads(sys.argv[2])
+    print(clothes["clothes"][2]["bodyparts"])
+    #print(sys.argv[2][:3])
