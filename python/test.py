538dd84dcd4d2f7357989a2b3b5a8ec0450f83a1
Benoit <benoit.leguay@gmail.com>
2019-01-11 19:16:37 +0100

    server update 9

--- python/test.py
+++ python/test.py
@@ -1,13 +1,2 @@
-import sys
-import json
-
-def test():
-	return "works on python"
-
 if __name__ == "__main__":
-    #functions = {'test': test}
-    #res_json = functions[sys.argv[1]]()
-
-    clothes = json.loads(sys.argv[2])
-    print(clothes["clothes"][2]["bodyparts"])
-    #print(sys.argv[2][:3])
+    print("eeesseseses")
