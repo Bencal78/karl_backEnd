01d4e689d79eabc83244e3b10a876e0088438fa1
Benoit <benoit.leguay@gmail.com>
2019-01-11 19:07:09 +0100

    server update 8

--- python/nodejs_communicator.py
+++ python/nodejs_communicator.py
@@ -11,11 +11,12 @@ def return_outfit():
     return outfits
 
 if __name__ == "__main__":
-    print("eee")
-    return
+    print("eeess")
+    """
     functions = {'return_outfit': return_outfit}
     try:
         res_json = functions[sys.argv[1]]()
     except Exception as e:
             print(e)
     print({"outfit": res_json})
+    """
