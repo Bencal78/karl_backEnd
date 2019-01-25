f5d4174949c4fde8e9289b5efbfe0956809803de
Benoit <benoit.leguay@gmail.com>
2019-01-25 14:13:50 +0100

    debug on server

--- controllers/pyreq.js
+++ controllers/pyreq.js
@@ -67,7 +67,6 @@ let return_outfit_quizz_start = async (req, res, next) => {
 };
 
 let return_outfit_rl = async(req, res, next) => {
-  console.log("begin");
   var id = req.query.id
   if(!id){
     return res.status(404)
@@ -90,10 +89,13 @@ let return_outfit_rl = async(req, res, next) => {
     user["last_taste"] = user.tastes.slice(-1)[0]
   }
   var clothes_json = {"clothes": user.clothes, "rl_cat_score": user.rl_cat_score, "last_taste": user.last_taste}
+  console.log(clothes_json.last_taste);
+  console.log(clothes_json.rl_cat_score);
   var clothes_string = JSON.stringify(clothes_json)
   var py = spawn('python3', ['./python/nodejs_communicator.py', req.query.func_name, clothes_string])// nodejs_communicator
   /*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
   py.stdout.on('data', (data) => {
+    console.log(data.toString().replace(/'/g, '"'));
     result = JSON.parse(data.toString().replace(/'/g, '"'))
     return res.status(200).json(result["outfit"]);
   });
